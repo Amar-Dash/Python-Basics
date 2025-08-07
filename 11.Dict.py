77665f3552d7ccc7361413e5fb852a60a0854e4c
Hackerboy <webdevloper206@gmail.com>
2025-08-07 07:06:21 +0530

    Dict 2

--- 11.Dict.py
+++ 11.Dict.py
@@ -71,3 +71,109 @@ thisdict = {
   "year": 1964
 }
 thisdict.update({"year": 2020})
+
+#Remove items
+
+#by pop()
+# The pop() method removes the item with the specified key name
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.pop("model")
+print(thisdict)
+#The popitem() method removes the last inserted item
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.popitem()
+print(thisdict)
+#del
+# The del keyword removes the item with the specified key name
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+del thisdict["model"]
+print(thisdict)
+#clear()
+# The clear() method empties the dictionary
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+thisdict.clear()
+print(thisdict)
+
+#Loop
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+for x in thisdict:
+  print(thisdict[x])
+
+for x in thisdict:
+  print(x)
+
+#value()
+for x in thisdict.values():
+  print(x)
+
+#key()
+for x in thisdict.keys():
+  print(x)
+
+#items()
+for x, y in thisdict.items():
+  print(x, y)
+
+#Copying
+
+#copy()
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = thisdict.copy()
+print(mydict)
+#by constructor
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964
+}
+mydict = dict(thisdict)
+print(mydict)
+
+#Nested dicts
+myfamily = {
+  "child1" : {
+    "name" : "Emil",
+    "year" : 2004
+  },
+  "child2" : {
+    "name" : "Tobias",
+    "year" : 2007
+  },
+  "child3" : {
+    "name" : "Linus",
+    "year" : 2011
+  }
+}
+#Acseesing
+print(myfamily["child1"]['name'])
+
+#Looping
+for x, obj in myfamily.items():
+  print(x)
+
+  for y in obj:
+    print(y + ':', obj[y])
